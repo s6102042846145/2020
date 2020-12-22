5af73d1d6a216c471f01c8e593becbcc5cd0adf3
Dum-Soken <s6102042846145@email.kmutnb.ac.th>
2020-12-22 09:33:56 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ AHC_MLK/AHC_MLK/Controllers/Admin/BoardListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AHC_MLK.Controllers.Admin
+{
+    public class BoardListController : Controller
+    {
+        // GET: BoardList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
